d9b16f1182be517a39742a3f0f36a7ef99b9fa57
guiguitome <guitomef@gmail.com>
2025-02-23 13:03:43 -0300

    atualizando

--- ponteiro/produtos.c
+++ ponteiro/produtos.c
@@ -33,10 +33,10 @@ int main(){
         getchar();
         fgets(c[i].produto, 50, stdin);
 
-        printf("Digite a quantidade disponivel do %d° produto", i + 1);
+        printf("Digite a quantidade disponivel do %d° produto\n", i + 1);
         scanf("%d", &c[i].qtd);
 
-        printf("Digite o valor do produto %d", i + 1);
+        printf("Digite o valor do produto %d\n", i + 1);
         scanf("%f", &c[i].valor);
 
         if(c[i].valor > maior_preco){
@@ -53,7 +53,7 @@ int main(){
     printf("\n\n");
 
     printf("Produto com maior preço: %s - R$%.2f\n", preco, maior_preco);
-    printf("Produto com maior estoque; %s - %d", estoque, maior_etq);
+    printf("Produto com maior estoque: %s - %d", estoque, maior_etq);
 
     free(c);
     c = NULL;
